7d8dc7acbdd252d3b2ddba99b8c10f49a82ef503
SomethingOfValueLessThanThat <147081832+SomethingOfValueLessThanThat@users.noreply.github.com>
2023-10-05 23:28:54 +0200

    Add files via upload

---
+++ javascript/global.js
@@ -0,0 +1,27 @@
+// Custom cursor effect
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 2000, fill: "forwards" });
+}
+
+
+// Page transtion effect
+function handleClick(e) {
+  e.preventDefault();
+  const bars = document.querySelectorAll(".bar");
+  bars.forEach((bar, i) => {
+    bar.style.animationPlayState = "running";
+  });
+  const lastBar = bars[bars.length - 1];
+  lastBar.addEventListener("animationend", () => {
+    setTimeout(() => {
+      window.location = e.target.href;
+    }, 500);
+  });
+}
