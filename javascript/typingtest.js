4200ca8a0c8ca9afd9858852b20079944fcda6e0
SomethingOfValueLessThanThat <147081832+SomethingOfValueLessThanThat@users.noreply.github.com>
2023-10-07 20:31:20 +0200

    Add files via upload

--- javascript/typingtest.js
+++ javascript/typingtest.js
@@ -88,15 +88,15 @@ tryAgainBtn.addEventListener("click", resetGame);
 
 // Page transtion effect
 function handleClick(e) {
-  e.preventDefault();
-  const bars = document.querySelectorAll(".bar");
-  bars.forEach((bar, i) => {
-    bar.style.animationPlayState = "running";
-  });
-  const lastBar = bars[bars.length - 1];
-  lastBar.addEventListener("animationend", () => {
-    setTimeout(() => {
-      window.location = e.target.href;
-    }, 500);
-  });
-}
+    e.preventDefault();
+    const bars = document.querySelectorAll(".bar");
+    bars.forEach((bar, i) => {
+      bar.style.animationPlayState = "running";
+    });
+    const lastBar = bars[bars.length - 1];
+    lastBar.addEventListener("animationend", () => {
+      setTimeout(() => {
+        window.location = e.target.href;
+      }, 500);
+    });
+  }
